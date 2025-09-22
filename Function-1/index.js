ebc52f761c43d8c00b5045e1f7734356dc86efc0
Logezz-07 <logeshwaran.b@servion.com>
2025-09-22 17:54:25 +0530

    Update index.js

--- Function-1/index.js
+++ Function-1/index.js
@@ -16,7 +16,7 @@ functions.http('helloHttp', async (req, res) => {
         let sessionParams = {};
 
         if (tag === "callStartApi") {
-            console.log("CallStartAPI INVOKED");
+            
             const apiUrl = "https://dev1-cct.rogers.com/dev01-config/ivr-config-ms/ivr/8335293704/call-start/params";
             const headers = {
                 cdr: "112432245667775757",
