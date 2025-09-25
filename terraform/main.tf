91e629e93ad1f669d42ebb0c1b2df4bb0a74b0f2
Logezz-07 <logeshwaran.b@servion.com>
2025-09-25 23:15:04 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,15 +18,6 @@ provider "google" {
   region  = var.region
 }
 
-resource "google_storage_bucket_object" "function_objects" {
-  for_each    = toset(var.functions)
-  name        = "${each.key}.zip"
-  bucket      = google_storage_bucket.function_bucket.name
-  source      = data.archive_file.functions[each.key].output_path
-
-  # 👇 This makes Terraform detect code changes and re-upload
-  source_hash = filemd5(data.archive_file.functions[each.key].output_path)
-}
 # Archive each function source
 data "archive_file" "functions" {
   for_each    = toset(var.functions)
@@ -41,7 +32,7 @@ resource "google_storage_bucket_object" "function_objects" {
   name        = "${each.key}.zip"
   bucket      = google_storage_bucket.function_bucket.name
   source      = data.archive_file.functions[each.key].output_path
-  content_md5 = filemd5(data.archive_file.functions[each.key].output_path)
+  source_hash = filemd5(data.archive_file.functions[each.key].output_path)
 }
 
 # Deploy Cloud Functions
