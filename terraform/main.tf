47797bedfebac0e6b7d606d91e21a86f99830115
Logezz-07 <logeshwaran.b@servion.com>
2025-09-25 18:08:49 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,35 +18,32 @@ provider "google" {
   region  = var.region
 }
 
-# Random suffix for new bucket
 resource "random_id" "bucket" {
   byte_length = 4
 }
 
-# Storage bucket for function sources
 resource "google_storage_bucket" "function_bucket" {
   name                        = "${var.project_id}-gcf-source-${random_id.bucket.hex}"
   location                    = var.region
   uniform_bucket_level_access = true
 }
 
-# Archive function source code
 data "archive_file" "functions" {
   for_each    = toset(var.functions)
   type        = "zip"
   output_path = "/tmp/${each.key}.zip"
-  source_dir  = "../${each.key}"  # relative path from terraform folder
+  source_dir  = "../${each.key}"
 }
 
-# Upload each zip to bucket
 resource "google_storage_bucket_object" "function_objects" {
   for_each = toset(var.functions)
-  name     = "${each.key}.zip"
+  
+  # Append hash of archive content to object name to force update on change
+  name     = "${each.key}-${data.archive_file.functions[each.key].output_sha}.zip"
   bucket   = google_storage_bucket.function_bucket.name
   source   = data.archive_file.functions[each.key].output_path
 }
 
-# Create Cloud Functions
 resource "google_cloudfunctions2_function" "functions" {
   for_each    = toset(var.functions)
   name        = each.key
@@ -67,14 +64,12 @@ resource "google_cloudfunctions2_function" "functions" {
 
   service_config {
     min_instance_count = 1
-    max_instance_count = 1
     available_memory   = "256M"
     timeout_seconds    = 60
     ingress_settings   = "ALLOW_INTERNAL_ONLY"
   }
 }
 
-# Allow public HTTP invoke
 resource "google_cloud_run_service_iam_member" "member" {
   for_each = google_cloudfunctions2_function.functions
 
@@ -86,7 +81,6 @@ resource "google_cloud_run_service_iam_member" "member" {
   depends_on = [google_cloudfunctions2_function.functions]
 }
 
-# Output function URLs
 output "function_uris" {
   value = {
     for k, f in google_cloudfunctions2_function.functions :
